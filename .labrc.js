9fedf93b819391562ef53d955f32879a0ba36299
Francesco Pezzella <franzpezzella@gmail.com>
2018-12-04 14:41:10 +0100

    labrc

---
+++ .labrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  timeout: 0,
+  assert: 'code',
+  threshold: 100,
+  lint: true,
+  verbose: true,
+  globals: 'SharedArrayBuffer,Atomics,BigUint64Array,BigInt64Array,BigInt,URL,URLSearchParams',
+  coverage: true
+};
