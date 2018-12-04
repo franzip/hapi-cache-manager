38bdddffb83dee6052e1a7bbbba19e101763e4ed
Francesco Pezzella <franzpezzella@gmail.com>
2018-12-04 14:14:13 +0100

    bump dependencies

--- test/index.js
+++ test/index.js
@@ -5,6 +5,7 @@ const Code = require('code');
 const Hapi = require('hapi');
 const JsonServer = require('json-server');
 const Request = require('request');
+
 const lab = exports.lab = Lab.script();
 
 const expect = Code.expect;
